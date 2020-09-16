c3a21b0b700356e6eb1544c3c087339421387de7
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-09-16 17:31:24 +0100

    adding player class with tests

---
+++ EscapeMines/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+namespace EscapeMines
+{
+    public class Player
+    {
+        private readonly Board board;
+        public Player(Board board)
+        {
+            this.board = board;
+        }
+
+        public Result Play(Turtle turtle, IEnumerable<Action> actionSequence)
+        {
+            Result result = board.GetResultOfCoordinate(turtle.Coordinate);
+            foreach(Action action in actionSequence)
+            {
+                if (result != Result.StillInDanger) break;
+                switch (action)
+                {
+                    case Action.Move:
+                        turtle.Move();
+                        break;
+                    case Action.RotateRight:
+                        turtle.RotateRight();
+                        break;
+                    case Action.RotateLeft:
+                        turtle.RotateLeft();
+                        break;
+                    default:
+                        throw new InvalidOperationException("Action was not recognised");
+                }
+                result = board.GetResultOfCoordinate(turtle.Coordinate);
+            }
+            return result;
+        }
+    }
+}
---
+++ EscapeMinesTests/PlayerTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+using EscapeMines;
+using System.Collections.Generic;
+using Action = EscapeMines.Action;
+namespace EscapeMinesTests
+{
+    public class PlayerTests
+    {
+        public static IEnumerable<object[]> GetTestInputs()
+        {
+            yield return new object[] { Result.MineHit,
+                new List<Action> {
+                    Action.RotateRight,
+                    Action.Move,
+                    Action.RotateLeft,
+                    Action.Move,
+                    Action.Move,
+                }
+            };
+
+            yield return new object[] { Result.Success,
+                new List<Action> {
+                    Action.Move,
+                    Action.RotateRight,
+                    Action.Move,
+                    Action.Move,
+                    Action.RotateRight,
+                    Action.Move,
+                    Action.Move,
+                    Action.RotateLeft,
+                    Action.Move,
+                    Action.Move,
+                }
+            };
+
+            yield return new object[] { Result.OffBoard,
+                new List<Action> {
+                    Action.Move,
+                    Action.RotateRight,
+                    Action.Move,
+                    Action.Move,
+                    Action.Move,
+                    Action.Move,
+                    Action.Move,
+                    Action.Move,
+                }
+            };
+
+            yield return new object[] { Result.StillInDanger,
+                new List<Action> {
+                    Action.RotateRight,
+                    Action.RotateRight,
+                    Action.Move,
+                    Action.Move,
+                    Action.RotateLeft,
+                    Action.Move,
+                    Action.Move,
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestInputs))]
+        public void PlayerPlaysTheActionSequenceCorrectly(Result expectedResult, IEnumerable<Action> actionSequence)
+        {
+            Board board = new Board(4, 5, new Coordinate(2, 4), new List<Coordinate> {
+                new Coordinate(1, 1),
+                new Coordinate(1, 3),
+                new Coordinate(3, 3)
+            });
+
+            Player player = new Player(board);
+
+            Turtle turtle = new Turtle(new Coordinate(1, 0), Direction.North);
+
+            Result result = player.Play(turtle, actionSequence);
+
+            result.ShouldBe(expectedResult);
+        }
+    }
+}
