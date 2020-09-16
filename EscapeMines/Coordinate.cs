c069a98cde77304c9102a7cb9a3e5e63236fe518
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-09-16 11:47:22 +0100

    Addig coordinate class with tests

---
+++ EscapeMines/Coordinate.cs
@@ -0,0 +1,18 @@
+﻿namespace EscapeMines
+{
+    public class Coordinate
+    {
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+
+        public override int GetHashCode() => $"{X}-{Y}".GetHashCode();
+        public override bool Equals(object obj)
+            => obj is Coordinate coordinate && coordinate.GetHashCode() == GetHashCode();
+    }
+}
---
+++ EscapeMinesTests/CoordinateTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+using EscapeMines;
+namespace EscapeMinesTests
+{
+    public class CoordinateTests
+    {
+        [Fact]
+        public void CoordinateConstructorWorks()
+        {
+            Coordinate coordinate = new Coordinate(1, 3);
+            coordinate.X.ShouldBe(1);
+            coordinate.Y.ShouldBe(3);
+        }
+
+        [Fact]
+        public void CoordinateEqualityWorks()
+        {
+            Coordinate a = new Coordinate(1, 3);
+            Coordinate b = new Coordinate(1, 3);
+            Coordinate c = new Coordinate(1, 4);
+            Coordinate d = new Coordinate(3, 1);
+
+            a.ShouldBe(b);
+            a.ShouldNotBe(c);
+            a.ShouldNotBe(d);
+        }
+    }
+}
