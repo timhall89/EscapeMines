ceb707de832114a73eedc00ce8e5709239b77916
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-09-16 12:25:55 +0100

    adding Direction enum with tests

---
+++ EscapeMines/Direction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace EscapeMines
+{
+    public enum Direction
+    {
+        North, South, East, West
+    }
+
+    public static class DirectionExtensions
+    {
+        public static Direction GetDirectionToTheRight(this Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.North:
+                    return Direction.East;
+                case Direction.South:
+                    return Direction.West;
+                case Direction.East:
+                    return Direction.South;
+                case Direction.West:
+                    return Direction.North;
+                default:
+                    throw new ArgumentOutOfRangeException("Direction not recognised");
+            }
+        }
+
+        public static Direction GetDirectionToTheLeft(this Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.North:
+                    return Direction.West;
+                case Direction.South:
+                    return Direction.East;
+                case Direction.East:
+                    return Direction.North;
+                case Direction.West:
+                    return Direction.South;
+                default:
+                    throw new ArgumentOutOfRangeException("Direction not recognised");
+            }
+        }
+    }
+}
---
+++ EscapeMinesTests/DirectionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+using EscapeMines;
+namespace EscapeMinesTests
+{
+    public class DirectionTests
+    {
+        [Theory]
+        [InlineData(Direction.North, Direction.East)]
+        [InlineData(Direction.South, Direction.West)]
+        [InlineData(Direction.East, Direction.South)]
+        [InlineData(Direction.West, Direction.North)]
+        public void GetDirectionToTheRightExtentionMethodWorks(Direction start, Direction toTheRight)
+        {
+            start.GetDirectionToTheRight().ShouldBe(toTheRight);
+        }
+
+        [Theory]
+        [InlineData(Direction.North, Direction.West)]
+        [InlineData(Direction.South, Direction.East)]
+        [InlineData(Direction.East, Direction.North)]
+        [InlineData(Direction.West, Direction.South)]
+        public void GetDirectionToTheLeftExtentionMethodWorks(Direction start, Direction toTheLeft)
+        {
+            start.GetDirectionToTheLeft().ShouldBe(toTheLeft);
+        }
+    }
+}
