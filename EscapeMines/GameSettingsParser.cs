9ffd1014f8045378ebe414419e15b859cdb8bf32
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-09-16 19:49:25 +0100

    refactoring the parsing method to make it clearer

--- EscapeMines/GameSettingsParser.cs
+++ EscapeMines/GameSettingsParser.cs
@@ -16,18 +16,11 @@ namespace EscapeMines
         {
             try
             {
-                string[] boardSizes = settingsLines[0].Split(' ');
-                int boardSizeX = int.Parse(boardSizes[0]);
-                int boardSizeY = int.Parse(boardSizes[1]);
+                (int boardSizeX, int boardSizeY) = ParseBoardSize(settingsLines[0]);
 
-                string[] mineCoordinates = settingsLines[1].Split(' ');
-                IEnumerable<Coordinate> mines = mineCoordinates.Select(mineCoordinate => {
-                    string[] coordinateSplit = mineCoordinate.Split(',');
-                    return new Coordinate(int.Parse(coordinateSplit[0]), int.Parse(coordinateSplit[1]));
-                });
+                IEnumerable<Coordinate> mines = ParseMines(settingsLines[1]);
 
-                string[] exitCoordinate = settingsLines[2].Split(' ');
-                Coordinate exit = new Coordinate(int.Parse(exitCoordinate[0]), int.Parse(exitCoordinate[1]));
+                Coordinate exit = ParseExitCoordinate(settingsLines[2]);
 
                 string[] turtleSettings = settingsLines[3].Split(' ');
                 Turtle turtle = new Turtle(
@@ -36,12 +29,7 @@ namespace EscapeMines
                         int.Parse(turtleSettings[1])),
                     ParseDirection(turtleSettings[2]));
 
-                List<IEnumerable<Action>> actionSequences = new List<IEnumerable<Action>>();
-                for (int i = 4; i < settingsLines.Length; i++)
-                {
-                    IEnumerable<Action> actionSequence = settingsLines[i].Split(' ').Select(action => ParseAction(action));
-                    actionSequences.Add(actionSequence);
-                };
+                IEnumerable<IEnumerable<Action>> actionSequences = ParseActionSequences(settingsLines);
 
                 return new GameSettings(boardSizeX, boardSizeY, mines, exit, turtle, actionSequences);
             }
@@ -50,6 +38,37 @@ namespace EscapeMines
                 throw new InvalidDataException("Part of the settings data is invald");
             }
 
+        }
+
+        private static (int, int) ParseBoardSize(string boardSize)
+        {
+            string[] boardSizes = boardSize.Split(' ');
+            return (int.Parse(boardSizes[0]), int.Parse(boardSizes[1]));
+        }
+
+        private static IEnumerable<Coordinate> ParseMines(string mineCoordinates)
+        {
+            string[] mineCoordinatesSplit = mineCoordinates.Split(' ');
+            return mineCoordinatesSplit.Select(mineCoordinate => {
+                string[] coordinateSplit = mineCoordinate.Split(',');
+                return new Coordinate(int.Parse(coordinateSplit[0]), int.Parse(coordinateSplit[1]));
+            });
+        }
+
+        private static Coordinate ParseExitCoordinate(string exitCoordinate)
+        {
+            string[] exitCoordinateSplit = exitCoordinate.Split(' ');
+            return new Coordinate(int.Parse(exitCoordinateSplit[0]), int.Parse(exitCoordinateSplit[1]));
+        }
+
+        private static Turtle ParseTurtle(string turtle)
+        {
+            string[] turtleSettings = turtle.Split(' ');
+            return new Turtle(
+                new Coordinate(
+                    int.Parse(turtleSettings[0]),
+                    int.Parse(turtleSettings[1])),
+                ParseDirection(turtleSettings[2]));
         }
 
         private static Direction ParseDirection(string direction)
@@ -63,6 +82,17 @@ namespace EscapeMines
                 default:
                     throw new ArgumentOutOfRangeException(null, $"Direction [{direction}] not recognised");
             }
+        }
+
+        private static IEnumerable<IEnumerable<Action>> ParseActionSequences(string[] settingsLines)
+        {
+            List<IEnumerable<Action>> actionSequences = new List<IEnumerable<Action>>();
+            for (int i = 4; i < settingsLines.Length; i++)
+            {
+                IEnumerable<Action> actionSequence = settingsLines[i].Split(' ').Select(action => ParseAction(action));
+                actionSequences.Add(actionSequence);
+            };
+            return actionSequences;
         }
 
         private static Action ParseAction(string action)
