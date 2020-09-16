5e107ec93d800f44bd947cef7dd8010b9d4b9e6c
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-09-16 15:36:56 +0100

    adding Turtle class with tests

---
+++ EscapeMines/Turtle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace EscapeMines
+{
+    public class Turtle
+    {
+        public Turtle(Coordinate coordinate, Direction direction)
+        {
+            Coordinate = coordinate;
+            Direction = direction;
+        }
+
+        public Coordinate Coordinate { get; }
+        public Direction Direction { get; private set; }
+
+        public void RotateRight()
+        {
+            Direction = Direction.GetDirectionToTheRight();
+        }
+
+        public void RotateLeft()
+        {
+            Direction = Direction.GetDirectionToTheLeft();
+        }
+
+        public void Move()
+        {
+            switch (Direction)
+            {
+                case Direction.North:
+                    Coordinate.X--;
+                    break;
+                case Direction.South:
+                    Coordinate.X++;
+                    break;
+                case Direction.East:
+                    Coordinate.Y++;
+                    break;
+                case Direction.West:
+                    Coordinate.Y--;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(null, "Direction is not valid");
+            }
+        }
+    }
+}
---
+++ EscapeMinesTests/TurtleTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xunit;
+using Shouldly;
+using EscapeMines;
+namespace EscapeMinesTests
+{
+    public class TurtleTests
+    {
+        [Fact]
+        public void TurleConstructedCorrectly()
+        {
+            Coordinate coordinate = new Coordinate(1, 5);
+            Turtle turtle = new Turtle(coordinate, Direction.South);
+            turtle.Coordinate.ShouldBe(coordinate);
+            turtle.Direction.ShouldBe(Direction.South);
+        }
+
+        [Fact]
+        public void TurtleRotateRightCorrectly()
+        {
+            Turtle turtle = new Turtle(new Coordinate(0, 0), Direction.South);
+
+            turtle.RotateRight();
+
+            turtle.Direction.ShouldBe(Direction.West);
+        }
+
+        [Fact]
+        public void TurtleRotateLeftCorrectly()
+        {
+            Turtle turtle = new Turtle(new Coordinate(0, 0), Direction.South);
+
+            turtle.RotateLeft();
+
+            turtle.Direction.ShouldBe(Direction.East);
+        }
+
+        [Theory]
+        [InlineData(0, 0, Direction.North, -1, 0)]
+        [InlineData(7, 0, Direction.South, 8, 0)]
+        [InlineData(6, 5, Direction.East, 6, 6)]
+        [InlineData(1, 3, Direction.West, 1, 2)]
+        public void TurtleMoveCorrectly(int startX, int startY, Direction direction, int movedX, int movedY)
+        {
+            Turtle turtle = new Turtle(new Coordinate(startX, startY), direction);
+
+            turtle.Move();
+
+            Coordinate expectedCoordinate = new Coordinate(movedX, movedY);
+            turtle.Coordinate.ShouldBe(expectedCoordinate);
+        }
+    }
+}
